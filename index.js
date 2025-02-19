1e44a6e84c78343c47e2ee0fec258db711e300a1
Riko <riko.fluchel@gmail.com>
2025-02-19 06:42:09 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your code here
+
+class Breakfast {
+    constructor(food, drink) {
+        this.food = food;
+        this.drink = drink
+    }
+}
+
+class Lunch {
+    constructor(salad, soup, drink){
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink
+    }
+}
+
+class Dinner {
+    #dessert;
+    constructor(salad, soup, entree, dessert){
+        this.#dessert = dessert;
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree
+    }
+}
+
+let mondayBreakfast = new Breakfast("scrambled eggs", "black tea")
+let mondayLunch = new Lunch("argulula, goat cheese and cranberries", "potato leek soup", "iced tea")
+let mondayDinner = new Dinner("caesar salad", "chicken noodle soup", "steak", "fudge sundae")
